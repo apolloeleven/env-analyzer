8ab7198204ac7d45d80399ea34d1615ec8a1891a
Saiat Kalbiev <kalbievich11@gmail.com>
2018-04-02 14:26:52 +0400

    Create file not found and invalid file exceptions

---
+++ exceptions/FileNotFoundException.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sai
+ * Date: 4/2/18
+ * Time: 1:53 PM
+ * @author Saiat Kalbiev <kalbievich11@gmail.com>
+ */
+
+namespace apollo11\envAnalizer\exceptions;
+
+
+class FileNotFoundException extends \Exception
+{
+    public function __construct($message, $code = 0, Exception $previous = null) {
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function __toString() {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
+
+}
